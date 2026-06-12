13857252956dff86dbc8ee1a32c982ac36c19699
Fidel Anyanwu <fanyanwu@mcneese.edu>
2026-06-12 11:34:45 -0500

    Polish fitness site links and assets

--- js/custom.js
+++ js/custom.js
@@ -137,29 +137,15 @@ $(document).ready(function () {
 });
 
 
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-36251023-1']);
-_gaq.push(['_setDomainName', 'jqueryscript.net']);
-_gaq.push(['_trackPageview']);
-
-(function () {
-  var ga = document.createElement('script');
-  ga.type = 'text/javascript';
-  ga.async = true;
-  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(ga, s);
-})();
-
-
-
 // define all UI variable
 const navToggler = document.querySelector('.nav-toggler');
 const navMenu = document.querySelector('.site-navbar ul');
 const navLinks = document.querySelectorAll('.site-navbar a');
 
 // load all event listners
-allEventListners();
+if (navToggler && navMenu) {
+  allEventListners();
+}
 
 // functions of all event listners
 function allEventListners() {
